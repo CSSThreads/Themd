c652ddf75882ce45d46fe412c03094851581ad80
Jasiukiewicz Tymon <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-10-26 19:08:03 +0200

    Create default.js

---
+++ default.js
@@ -0,0 +1,61 @@
+var converter = window.markdownit({
+  html: true,
+  linkify: true
+}).use(window.markdownItAttrs, {
+  allowedAttributes: ['id', 'class', 'style']
+}).use(window.markdownitEmoji).use(window.markdownitSub).use(window.markdownitSup).use(taskListModule).use(headersId).use(containerModule, 'dropdown', {
+  validate: function(params) {
+    return params.trim().match(/^(.*)$/);
+  },
+
+  render: function(tokens, idx) {
+    var m = tokens[idx].info.trim().match(/^(.*)$/);
+
+    if (tokens[idx].nesting === 1 && m[0].startsWith('dropdown')) {
+      return '<details><summary>' + m[0] + '</summary>\n';
+
+    } else {
+      // closing tag
+      return '</details>\n';
+    }
+  }
+}).use(containerModule, 'decorate', {
+  validate: function(params) {
+    return params.trim().match(/^(.*)$/);
+  },
+
+  render: function(tokens, idx) {
+    var m = tokens[idx].info.trim().match(/^(.*)$/);
+
+    if (tokens[idx].nesting === 1) {
+      return '<div ' + m[0] + '>\n';
+
+    } else {
+      // closing tag
+      return '</div>\n';
+    }
+  }
+})
+
+var contentHttpReq = new XMLHttpRequest();
+var contentPath = window.location.search.slice(1).split('/')[0] == '' ? window.location.origin + '/pages/index.md' : window.location.origin + '/pages/' + window.location.search.slice(1).split('/')[0] + '.md';
+contentHttpReq.open("GET", contentPath, false);
+contentHttpReq.send(null);
+
+var md = contentHttpReq.responseText
+var themeName = md.split('\n').shift();
+
+var styleHttpReq = new XMLHttpRequest();
+styleHttpReq.open("GET", 'https://themd.jasiukiewicztym.repl.co/themes/themd.css', false);
+styleHttpReq.send(null);
+
+// appending style
+document.head.innerHTML += `<style>${styleHttpReq.responseText}</style>`
+
+md = md.split('\n').splice(1, md.length - 1).join('\n')
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#content').innerHTML = converter.render(md)
+})
+
+hljs.highlightAll();
