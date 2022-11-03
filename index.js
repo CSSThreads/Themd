b38bd68ab9dc7288fa671d0edb1b58dc8050bc26
Jasiukiewicz Tymon <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-11-03 21:34:19 +0100

    🛠 - Header id to do

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var hljs = require('highlight.js');
+const string = require('string')
+const slugify = s => string(s).slugify().toString()
+
+mir = require('markdown-it-replace-it');
+mir.replacements.push({ name: '©', re: /\([c|C]\)/g, html: true, sub: function (s) { return '©'; }, default: true });
+mir.replacements.push({ name: '®', re: /\([r|R]\)/g, html: true, sub: function (s) { return '®'; }, default: true });
+mir.replacements.push({ name: '™', re: /\([t|T][m|M]\)/g, html: true, sub: function (s) { return '™'; }, default: true });
+mir.replacements.push({ name: '℗', re: /\([p|P]\)/g, html: true, sub: function (s) { return '℗'; }, default: true });
+
+mir.replacements.push({ name: '±', re: /\+-/g, html: true, sub: function (s) { return '±'; }, default: true });
+mir.replacements.push({ name: '→', re: /->/g, html: true, sub: function (s) { return '→'; }, default: true });
+mir.replacements.push({ name: '⇒', re: /=>/g, html: true, sub: function (s) { return '⇒'; }, default: true });
+mir.replacements.push({ name: '←', re: /<-/g, html: true, sub: function (s) { return '←'; }, default: true });
+mir.replacements.push({ name: '⇐', re: /<=/g, html: true, sub: function (s) { return '⇐'; }, default: true });
 
 var MarkdownIt = require('markdown-it')
 var converter = new MarkdownIt({
     html: true,
     linkify: true
-}).use(require('markdown-it-attrs'), {
+}).use(mir).use(require('markdown-it-anchor'), { slugify }).use(require('markdown-it-attrs'), {
     allowedAttributes: ['id', 'class', 'style']
 }).use(require('markdown-it-emoji')).use(require('markdown-it-sub')).use(require('markdown-it-sup')).use(require('markdown-it-task-lists')).use(require('markdown-it-container'), 'dropdown', {
     validate: function(params) {
@@ -40,6 +54,6 @@ var converter = new MarkdownIt({
     }
 })
   
-const md = "::: warning\nspoiler click me\n**content**\n:::\n"
+const md = "---\n__Hi :)__\n\n---\n::: warning\nspoiler => ->  +-fsdsfd(c)(tm) (c) (C) click me\n**content**\n:::\n# dfsdfs\n"
 
 console.log(converter.render(md))
